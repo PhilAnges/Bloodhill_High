55d772a5d9c8fd1b580d48c61ace560564e5817f
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2021-02-08 13:55:57 -0500

    Made 2 scripts
    Made a pause menu script and a menu navigation script.

---
+++ Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseUI;
+    public KeyCode pauseButton;
+    private bool isPaused;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPaused = false;
+        pauseUI.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(pauseButton))
+        {
+            isPaused = !isPaused;
+        }
+        if (isPaused == true)
+        {
+            pauseUI.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        if (isPaused == false)
+        {
+            pauseUI.SetActive(false);
+            Time.timeScale = 1f;
+        }
+    }
+
+    public void Unpause()
+    {
+        isPaused = false;
+    }
+}
