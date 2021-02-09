a4699c6bf1047dfabd3434ae4c4411117a54e73a
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2021-02-09 12:57:41 -0500

    Password
    Created a system where, if the player enters a hitbox, they can type in a password that, if valid, will disable a block.

---
+++ Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/AskForPassword.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AskForPassword : MonoBehaviour
+{
+    public string playerTag;
+    public GameObject passwordPlease;
+    public string thisIsPassword;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        passwordPlease.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(thisIsPassword == passwordPlease.GetComponent<TypePasscode>().password)
+        {
+            passwordPlease.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            passwordPlease.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            passwordPlease.SetActive(false);
+        }
+    }
+}
---
+++ Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/TypePasscode.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypePasscode : MonoBehaviour
+{
+    public Text fromKeyboard;
+    public string password;
+
+    void Start()
+    {
+        fromKeyboard = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        foreach (char c in Input.inputString)
+        {
+            if (c == '\b') // has backspace/delete been pressed?
+            {
+                if (fromKeyboard.text.Length != 0)
+                {
+                    fromKeyboard.text = fromKeyboard.text.Substring(0, fromKeyboard.text.Length - 1);
+                }
+            }
+            else if ((c == '\n') || (c == '\r')) // enter/return
+            {
+                password = fromKeyboard.text;
+                print(password);
+            }
+            else
+            {
+                fromKeyboard.text += c;
+            }
+        }
+    }
+}
