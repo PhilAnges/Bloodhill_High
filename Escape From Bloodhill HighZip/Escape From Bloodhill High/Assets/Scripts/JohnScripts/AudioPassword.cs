d064501f79bc812b54a2f83d302d5202a3f6f103
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2021-02-10 14:12:01 -0500

    Added Audio puzzle
    Added a script that waits for an audio file to finish playing before opening a door.

---
+++ Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/AudioPassword.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPassword : MonoBehaviour
+{
+    public string playerTag;
+    public AudioSource audioPassword;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            if(other.gameObject.GetComponent<ItemPickup>().pickedUpItem == true)
+            {
+                audioPassword.Play();
+                
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            audioPassword.Stop();
+        }
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            if (other.gameObject.GetComponent<ItemPickup>().pickedUpItem == true)
+            {
+                if (audioPassword.isPlaying == false)
+                {
+                    gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
