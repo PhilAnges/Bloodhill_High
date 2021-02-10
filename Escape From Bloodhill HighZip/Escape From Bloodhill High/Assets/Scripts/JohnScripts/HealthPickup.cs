252de8b714bf3a6c9bc6594326bdda9d787d76bc
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2021-02-10 15:06:15 -0500

    Health and Healing System
    Created a health system for the player where, if their current health drops to 0, they are sent to the main menu. Also created health pickups that heal the player when walked over.

---
+++ Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/HealthPickup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour
+{
+    public string playerTag;
+    public int heal;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            other.gameObject.GetComponent<PlayerHealth>().currentHealth = other.gameObject.GetComponent<PlayerHealth>().currentHealth + heal;
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        if(currentHealth <= 0)
+        {
+            SceneManager.LoadScene("MainMenu");
+        }
+    }
+}
