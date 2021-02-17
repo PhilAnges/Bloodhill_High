74c960cf265d5bf5211186327bfa796fce1447f1
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2021-02-17 15:35:26 -0500

    More work on the Inventory Screen.

--- Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/MenuScripts/InventoryMenu.cs
+++ Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/MenuScripts/InventoryMenu.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class InventoryMenu : MonoBehaviour
 {
+    public RectTransform[] buttonLocations;
+    public GameObject itemSlot;
+    public GameObject playerCharacter;
+
+    private int itemCounter;
+    private GameObject[] itemHold;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerCharacter = GameObject.FindGameObjectWithTag("Player");
+        itemCounter = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        itemHold = playerCharacter.GetComponent<ItemPickup>().inventory;
+        if (itemHold[itemCounter] != null)
+        {
+            Instantiate(itemSlot, buttonLocations[itemCounter]);
+            itemCounter++;
+        }
     }
 }
