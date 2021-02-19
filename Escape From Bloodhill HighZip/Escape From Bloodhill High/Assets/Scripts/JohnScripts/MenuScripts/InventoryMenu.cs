db0fecdb8e3e77a9b9be910cdd3a30eb52a179c7
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2021-02-19 15:18:44 -0500

    Show Item Descriptions
    In Inventory Screen, pressing a button will show a description of the item.

--- Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/MenuScripts/InventoryMenu.cs
+++ Escape From Bloodhill HighZip/Escape From Bloodhill High/Assets/Scripts/JohnScripts/MenuScripts/InventoryMenu.cs
@@ -9,6 +9,8 @@ public class InventoryMenu : MonoBehaviour
     public GameObject itemSlot;
     public GameObject playerCharacter;
 
+    public GameObject descriptionBox;
+
     private int itemCounter;
     public GameObject[] itemHold;
     // Start is called before the first frame update
@@ -30,7 +32,11 @@ public class InventoryMenu : MonoBehaviour
         itemHold = playerCharacter.GetComponent<ItemPickup>().inventory;
         if (itemHold[itemCounter] != null)
         {
+            itemSlot.GetComponent<ShowItemDescription>().itemHeld = itemHold[itemCounter];
+            itemSlot.GetComponent<ShowItemDescription>().descriptionBox = descriptionBox;
             itemSlot.GetComponentInChildren<Text>().text = itemHold[itemCounter].GetComponent<ItemProperties>().itemName;
+            
+            //descriptionBox.GetComponentInChildren<Text>().text = itemHold[itemCounter].GetComponent<ItemProperties>().itemDescription;
             Instantiate(itemSlot, buttonLocations[itemCounter]);
             itemCounter++;
         }
