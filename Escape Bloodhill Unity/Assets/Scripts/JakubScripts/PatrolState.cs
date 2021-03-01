13142c05c49ff91ad46de98bf8dbc7d7e9a32bf2
jsawczuk17 <38474667+jsawczuk17@users.noreply.github.com>
2021-03-01 13:37:26 -0500

    Path

--- Escape Bloodhill Unity/Assets/Scripts/JakubScripts/PatrolState.cs
+++ Escape Bloodhill Unity/Assets/Scripts/JakubScripts/PatrolState.cs
@@ -13,10 +13,11 @@ public class PatrolState : AIState
     {
         //Delete this line in final build
         parent.navAgent.speed = parent.patrolSpeed;
+
         parent.Sight();
         if (parent.navAgent.remainingDistance <= parent.arriveDistance)
         {
-            if (parent.nextPoint == parent.pathPoints.Length - 1 && parent.pathDirection == 1)
+            if (parent.nextPoint == parent.pathPoints.Count - 1 && parent.pathDirection == 1)
             {
                 parent.pathDirection *= -1;
             }
@@ -26,14 +27,14 @@ public class PatrolState : AIState
             }
             parent.previousPoint = parent.nextPoint;
             parent.nextPoint = parent.nextPoint + parent.pathDirection;
-            parent.navAgent.SetDestination(parent.pathPoints[parent.nextPoint].position);
+            parent.navAgent.SetDestination(parent.pathPoints[parent.nextPoint].gameObject.transform.position);
         }
         CheckConditions();
     }
 
     public override void EntryBehavior()
     {
-        parent.navAgent.SetDestination(parent.pathPoints[0].position);
+        parent.navAgent.SetDestination(parent.pathPoints[0].gameObject.transform.position);
         parent.navAgent.speed = parent.patrolSpeed;
         parent.alertTime = parent.ogAlertTime;
         Debug.Log("Entering Patrol State");
