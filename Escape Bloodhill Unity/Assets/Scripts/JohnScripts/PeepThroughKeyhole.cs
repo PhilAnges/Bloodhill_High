21f4544bbb2c2c13447db9ba440fc36818347bd5
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2021-03-04 14:20:12 -0500

    Peep Through Door
    When a player approaches a specific door and hold Q, the camera will switch to one on the other side of the door.

---
+++ Escape Bloodhill Unity/Assets/Scripts/JohnScripts/PeepThroughKeyhole.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PeepThroughKeyhole : MonoBehaviour
+{
+    public GameObject playerCamera;
+    public Camera doorCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
+        doorCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerCamera = GameObject.FindGameObjectWithTag("MainCamera");
+
+
+    }
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+            playerCamera.GetComponentInChildren<Camera>().enabled = false;
+            doorCamera.enabled = true;
+            }
+            if (Input.GetKeyUp(KeyCode.Q))
+            {
+            doorCamera.enabled = false;
+            playerCamera.GetComponentInChildren<Camera>().enabled = true;
+            }
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            doorCamera.enabled = false;
+            playerCamera.GetComponentInChildren<Camera>().enabled = true;
+        }
+    }
+}
