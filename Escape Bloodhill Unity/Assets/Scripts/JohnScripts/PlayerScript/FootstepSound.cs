f67c2a56fbed173cc67c553a2511cd29b7d2f73a
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2021-03-04 15:50:10 -0500

    Footstep Sounds
    Created script that plays footstep sounds while the player moves, and stops when the player does.

---
+++ Escape Bloodhill Unity/Assets/Scripts/JohnScripts/PlayerScript/FootstepSound.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepSound : MonoBehaviour
+{
+    public AudioSource walking;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            walking.Play();
+        }        
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            walking.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            walking.Play();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            walking.Play();
+        }
+        if (Input.anyKey == false)
+        {
+            walking.Stop();
+        }
+    }
+}
