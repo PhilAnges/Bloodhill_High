f1cbc58ddfa93fda34de75519a55b7bfb8ce32b7
PhilAnges <48360145+PhilAnges@users.noreply.github.com>
2021-03-05 17:56:21 -0500

    Journal Menu set up.

---
+++ Escape Bloodhill Unity/Assets/Scripts/JohnScripts/MenuScripts/JournalMenuControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JournalMenuControl : MonoBehaviour
+{
+    public GameObject descriptionBox;
+    public List<GameObject> journalEntries;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        journalEntries = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShowJournalEntry(int entry)
+    {
+        descriptionBox.GetComponentInChildren<Text>().text = journalEntries[entry].GetComponent<ItemProperties>().itemDescription;
+    }
+}
