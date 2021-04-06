a9adaf1fb8859120b0c8206591bd5cea5cbb6e22
jsawczuk17 <38474667+jsawczuk17@users.noreply.github.com>
2021-04-06 15:32:54 -0400

    Menu Update

--- Escape Bloodhill Unity/Assets/Scripts/JakubScripts/MenuCam.cs
+++ Escape Bloodhill Unity/Assets/Scripts/JakubScripts/MenuCam.cs
@@ -6,6 +6,7 @@ public class MenuCam : MonoBehaviour
 {
     public Transform startPosition, playPosition, helpPosition, quitPosition;
     public CamControl cam;
+    public GameObject yes, no;
     public int currentButton = 0;
 
 
@@ -18,23 +19,32 @@ public class MenuCam : MonoBehaviour
     {
         cam.target = playPosition;
         currentButton = 1;
+        yes.SetActive(true);
+        no.SetActive(true);
+
     }
 
     public void HelpButton()
     {
         cam.target = helpPosition;
         currentButton = 2;
+        yes.SetActive(true);
+        no.SetActive(true);
     }
 
     public void QuitButton()
     {
         cam.target = quitPosition;
         currentButton = 3;
+        yes.SetActive(true);
+        no.SetActive(true);
     }
 
     public void ResetPosition()
     {
         cam.target = startPosition;
         currentButton = 0;
+        yes.SetActive(false);
+        no.SetActive(false);
     }
 }
